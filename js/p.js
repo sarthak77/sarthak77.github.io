6edc00cd694175e48badd674a49b5245ef6a0510
sarthak singhal <sarthak.singhal@students.iiit.ac.in>
2020-05-14 14:36:44 +0530

    js speed fixed

--- js/p.js
+++ js/p.js
@@ -1,4 +1,5 @@
 // Parallax
+
 var image = document.getElementsByClassName('thumbnail');
 new simpleParallax(image,{
 	scale:2.1,
@@ -10,58 +11,41 @@ new simpleParallax(image,{
 
 var image = document.getElementsByClassName('parallax-headimg');
 new simpleParallax(image,{
-	scale:1.6,
+	scale:1.4,
 	orientation:'up',
 	delay:10,
 });
 
 // Text Animation
+
+var observer = new IntersectionObserver(function(entries)
+	{
+		var tg="."+entries[0].target.firstElementChild.className;
+		console.log(tg);
+		console.log(entries[0].intersectionRatio)
+		if(entries[0]['isIntersecting'] === true)
+		{
+			anime.timeline({loop: true})
+				.add({
+					targets: tg,
+					// targets: '.ml6 .letter',
+					translateY: ["0em", 0],//to translate above
+					translateZ: 0,
+					opacity:1,//darken the text
+					duration: 750,//duration of effect
+					delay: (el, i) => 75 * i
+					// delay: (el, i) => 15 * i
+				  })
+		}
+	}, { threshold:[0]});
+
+
 var textWrapper = document.querySelectorAll('.ml6 .letters');
 for (j=0;j<textWrapper.length;j++)
 {
-
 	tw=textWrapper[j];
-	console.log(tw);
-	var observer = new IntersectionObserver(function(entries){ console.log(entries);
-
-		if(entries[0]['isIntersecting'] === true)
-		{
-			// if(entries[0]['intersectionRatio'] === 1)
-			// {
-			// 	console.log("1");
-			// }
-			// else if(entries[0]['intersectionRatio'] > 0.5)
-			// {
-			// 	console.log(">0.5");
-			// }
-			// else
-			// {
-			// 	console.log("<0.5");
-			// }
-
-			anime.timeline({loop: true})
-			.add({
-				targets: '.ml6 .letter',
-				translateY: ["10em", 0],
-				// translateY: ["1.1em", 0],
-				translateZ: 0,
-				duration: 5,
-				opacity: 1,
-				// duration: 750,
-				// delay: (el, i) => 50 * i
-				delay: (el, i) => 25 * i
-			})
-		}
-		// else
-		// {
-		// 		console.log("0");
-		// }
-
-	}, { threshold: [0, 0.5, 1] });
-	
+	var s="<span class='letter"+String(j)+"'>$&</span>";
+	console.log(s);
+	tw.innerHTML = tw.textContent.replace(/\S/g,s);
 	observer.observe(tw);
-	tw.innerHTML = tw.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
 }
-
-
